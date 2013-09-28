cf62c4eb63717c4ec83c2de802834fae2323c639
bttf <reginaldchowdhury@gmail.com>
2013-09-28 02:23:58 +0000

    Refactored to use expressw

---
+++ app.js
@@ -0,0 +1,22 @@
+var port = 8000;
+var express = require('express'), app = express();
+var http = require('http')
+  , server = http.createServer(app);
+var engines = require('consolidate');
+var path = require('path');
+
+server.listen(port);
+
+app.enable('trust proxy');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
